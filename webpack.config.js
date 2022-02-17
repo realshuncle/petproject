e2c6e74523a5df6b991f7317087ff44ac24d6d7d
Vehcamut <vehcamut@gmail.com>
2022-02-17 08:42:25 +0400

    Adding a .css file to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path')
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, './dist'),        
-    },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+  filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),        
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
 //[name].bundle
