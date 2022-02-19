196ec2915ee387c80cd727737b0acc4aee36f18f
Vehcamut <vehcamut@gmail.com>
2022-02-19 16:22:26 +0400

    Checked available fonts

--- src/entry.js
+++ src/entry.js
@@ -4,6 +4,6 @@ function requireAll(requireContext) {
 
 requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
 requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
-//requireAll(require.context('./pages', true,/^\.\/(?!.*(?:__tests__)).*\.(scss?)$/));
+requireAll(require.context('./pages', true,/^\.\/(?!.*(?:__tests__)).*\.(scss?)$/));
 requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(scss?)$/));
 requireAll(require.context('./theme', true, /^\.\/(?!.*(?:__tests__)).*\.(scss?)$/));
