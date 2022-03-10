58271fefdd392800810a90abdb57d50349b6e509
Vehcamut <vehcamut@gmail.com>
2022-03-10 16:08:19 +0400

    Refactoring catalog.

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -5,53 +5,26 @@ import 'swiper/scss';
 import 'swiper/scss/effect-fade';
 import 'swiper/scss/autoplay';
 import 'swiper/scss/navigation';
-//import 'swiper/scss/pagination';
-//import { FALSE } from 'sass';
 
-const swiper = new Swiper('.catalog__swiper', {
-  // configure Swiper to use modules
-  //modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  effect: "fade",
-  //virtualTranslate: true,
-  fadeEffect: {
-    crossFade: true,
-  },
-  //setWrapperSize: false,
-  /*scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },*/
-  slidesPerView: 1,
-  //virtualTranslate: true,
-  //effect: 'coverflow',
-
-
- // slidersPerView: 1,
-  //freeMode: true,
-  allowTouchMove: false,
-  
-  //effect: "fade",
-  //
-  //fadeEffect: {
-  //  crossFade: true,
-  //},
-  //spaceBetween: 30,
-  //centeredSlides: true,
-  //autoplay: {
-    //delay: 5000,
-    //disableOnInteraction: false,
-  //},
-  loop: true,
-  speed: 500,
-
-
-});
+for (let swiperElement of document.querySelectorAll('.catalog__swiper')) {
+  const swiper = new Swiper(swiperElement, {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    slidesPerView: 1,
+    allowTouchMove: false,
+    loop: true,
+    speed: 1000,
+    loopPreventsSlide: false,
+  });
+}
