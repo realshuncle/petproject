d5f667be2cfb3f70d2c40aba9f65fdef51b13450
Vehcamut <vehcamut@gmail.com>
2022-03-10 13:37:52 +0400

    Catalog test page.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,13 @@
-//require('./index.html');
-//alert("lololo");
+var header = $('header'),
+		scrollPrev = 0;
+
+$(window).on('scroll', function() {
+	var scrolled = $(window).scrollTop();
+ 
+	if ( scrolled > 100 && scrolled > scrollPrev ) {
+		header.addClass('header__hidden');
+	} else {
+		header.removeClass('header__hidden');
+	}
+	scrollPrev = scrolled;
+});
