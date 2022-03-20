01b559da595c99c620436bbf8f50222858bd3e32
Vehcamut <vehcamut@gmail.com>
2022-03-20 13:40:35 +0400

    Added chart

---
+++ src/components/chart/chart.js
@@ -0,0 +1,66 @@
+import Chart from 'chart.js/auto';
+
+export default class DoughnatChart {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector('.chart__canvas')
+    this.colors = config.colors;
+    this.gradients = [];
+    this.data = config.data;
+    this.context = this.canvas.getContext('2d');
+    let temp = 0;
+    let sum = this.data.reduce((p,c) => p + c, 0);
+    for (let i = 0; i < 4; i++) {
+      let y0 = Math.round(60*(1 - Math.sin(Math.PI / 2 - temp * 2 * Math.PI/ sum )));
+      let x0 = Math.round(60*(1 - Math.cos(Math.PI / 2 + temp * 2 * Math.PI/ sum )));
+      temp += this.data[i];
+      let y1 = Math.round(60*(1 - Math.sin(Math.PI / 2 - temp * 2 * Math.PI/ sum )));
+      let x1 = Math.round(60*(1 - Math.cos(Math.PI / 2 + temp * 2 * Math.PI/ sum )));
+      this.gradients.push(this.context.createLinearGradient(Math.min(x0, x1), Math.min(y0, y1), Math.max(x0, x1), Math.max(y0, y1)));
+      this.gradients[i].addColorStop(0, this.colors[i][0]);
+      this.gradients[i].addColorStop(1, this.colors[i][1]);
+    }
+    this.chart = new Chart(this.canvas, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          borderAlign: 'inner',
+          hoverBorderColor: 'white',
+          label: 'My First Dataset',
+          data: this.data,
+          backgroundColor: [
+            this.gradients[0],
+            this.gradients[1],
+            this.gradients[2],
+            this.gradients[3],
+          ],
+          borderWidth: 1,
+        }]
+      },
+      options: {   
+        plugins: {
+          legend: {
+            display: false,
+            labels: {
+              usePointStyle: true,
+              boxWidth: 10,
+              font: {
+                size: 14,
+              },
+              backgroundColor: 'blue',         
+            },
+            position: 'right',
+            align: 'end',
+          },
+        },
+        events: null,
+        datasetRadiusBuffer: 50,
+        cutout: 53,
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    
+    });
+    this.element.querySelector('.chart__number').innerText = config.votes;
+  }
+}
